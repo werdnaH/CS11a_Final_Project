28b9cb5020f001b3f82828f6f67267c09922120c
werdnaH <32367543+werdnaH@users.noreply.github.com>
2017-11-01 23:11:27 -0400

    Add files via upload

---
+++ Block.java
@@ -0,0 +1,14 @@
+public class Block {
+  private boolean ic;// abbreviation of isclicked
+  private String s;
+  public Block(){
+    ic = false;
+    s = null;
+  }
+  public void click() {
+    System.out.println(s);
+  }
+  public void setFlag();
+  public void setqm(); //set question mark
+  
+}
