1b91232e8309fe1342065192b739765f6cc72121
LIZHAONAN <32943976+LIZHAONAN@users.noreply.github.com>
2017-11-15 22:47:10 -0500

    Add files via upload

--- LeaderBoard.java
+++ LeaderBoard.java
@@ -6,24 +6,43 @@
 *@author Zhaonan Li
 */
 import java.sql.*;
+
 public class LeaderBoard{
   public static void main(String[] args){
-    Connection c = null;
-    try {
-      Class.forName("org.sqlite.JDBC");
-      c = DriverManager.getConnection("jdbc:sqlite:leaderboard.db");
-    } catch ( Exception e ) {
-      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-      System.exit(0);
-    }
-    System.out.println("Opened database successfully");
+    createTable();
   }
-  public void dbConnect(){
-
+  //This method creates a table in the database named playData
+  public static void createTable(){
+	  Connection c = null;
+	  Statement stmt = null;
+	  try {
+		  Class.forName("org.sqlite.JDBC");
+		  c = DriverManager.getConnection("jdbc:sqlite:leaderboard.db");
+		  stmt = c.createStatement();
+		  String sql = "CREATE TABLE playData("
+		  		+ "id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,"
+		  		+ "userName TEXT UNIQUE,"
+		  		+ "date DATETIME,"
+		  		+ "time DOUBLE,"
+		  		+ "score INTEGER)";
+		  stmt.executeUpdate(sql);
+		  stmt.close();
+		  c.close();
+	  } catch (Exception e) {
+		  System.out.println("There's an error:"+e.getClass().getName()
+				  +": "+e.getMessage());
+		  System.exit(0);
+	  }
+	  System.out.println("Table created successfully!");
+  }
+  //This method update information inside playData 
+  public static void updateTable() {
+	  
   }
   //this method gives score based on time and level of difficulty
-
-  //public int scoreGenerator(double time, int size, int nb){
-
-  //}
+  public int scoreGenerator(double time, int size, int nb){
+	  int score;
+	  score = (int) time+size+nb;
+	  return score;
+  }
 }
