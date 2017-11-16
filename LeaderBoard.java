713435eba33badd58de6bc94b5bb35661d54c547
LIZHAONAN <32943976+LIZHAONAN@users.noreply.github.com>
2017-11-16 12:20:20 -0500

    Add files via upload

--- LeaderBoard.java
+++ LeaderBoard.java
@@ -9,19 +9,43 @@ import java.sql.*;
 
 public class LeaderBoard{
   public static void main(String[] args){
-    createTable();
+    //createPlayerTable();
+	  createDataTable();
   }
-  //This method creates a table in the database named playData
-  public static void createTable(){
+  //This method creates a table in the database for storing players' user names
+  public static void createPlayerTable(){
 	  Connection c = null;
 	  Statement stmt = null;
 	  try {
 		  Class.forName("org.sqlite.JDBC");
 		  c = DriverManager.getConnection("jdbc:sqlite:leaderboard.db");
 		  stmt = c.createStatement();
-		  String sql = "CREATE TABLE playData("
+		  String sql = "CREATE TABLE players("
 		  		+ "id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,"
-		  		+ "userName TEXT UNIQUE,"
+		  		+ "userName TEXT UNIQUE)";
+		  stmt.executeUpdate(sql);
+		  stmt.close();
+		  c.close();
+	  } catch (Exception e) {
+		  System.out.println("There's an error:"+e.getClass().getName()
+				  +": "+e.getMessage());
+		  System.exit(0);
+	  }
+	  System.out.println("Table created successfully!");
+  }
+  
+  //This method creates a table in the database for storing all relevant information
+  //for every game
+  public static void createDataTable(){
+	  Connection c = null;
+	  Statement stmt = null;
+	  try {
+		  Class.forName("org.sqlite.JDBC");
+		  c = DriverManager.getConnection("jdbc:sqlite:leaderboard.db");
+		  stmt = c.createStatement();
+		  String sql = "CREATE TABLE GameData("
+		  		+ "id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,"
+		  		+ "userName_id INTEGER"
 		  		+ "date DATETIME,"
 		  		+ "time DOUBLE,"
 		  		+ "score INTEGER)";
