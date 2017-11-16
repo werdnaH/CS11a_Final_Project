992587e2ea86f6020cc3cf5d5fd8011caecf12f8
李肇男 <32943976+LIZHAONAN@users.noreply.github.com>
2017-11-16 18:58:49 -0500

    Add files via upload

--- LeaderBoard.java
+++ LeaderBoard.java
@@ -12,18 +12,20 @@ public class LeaderBoard{
   //The main method is only for testing
   public static void main(String[] args){
 	LeaderBoard lb = new LeaderBoard();
-	lb.createDataTable();
+	lb.updateName("zhaonan");
   }
   
   public Connection c = null;
   public Statement stmt = null;
   public String sql = new String();//sql is command in SQL
   
+  //This method helps to connect to the leader board database and avoids
+  //duplicating code
   public void connect() {
 	  try {
 	  Class.forName("org.sqlite.JDBC");
 	  c = DriverManager.getConnection("jdbc:sqlite:leaderboard.db");
-	  System.out.println("Connected to database successfully...");
+	  //System.out.println("Connected to database successfully...");
 	  } catch (Exception e) {
 		  System.out.println("Connection fails because of an exception");
 		  System.out.println(e.getClass().getName() +
@@ -38,14 +40,61 @@ public class LeaderBoard{
 	  return score;
   }
   
-  //This method update information inside playData 
-  public void updateTable() {
+  public void updateName(String name) {
+	  if (nameId(name) != -1) {
+		  System.out.println("name exists");
+	  } else {
+		  try {
+			  connect();
+			  c.setAutoCommit(false);
+			  stmt = c.createStatement();
+			  sql = "INSERT INTO players (userName) " 
+			  		+"VALUES(\""+ name +"\");";
+			  stmt.executeUpdate(sql);
+			  stmt.close();
+			  c.commit();
+			  c.close();
+			  System.out.println("name inserted successfully");
+		  } catch(Exception e) {
+			  System.out.println("There's an error:"+e.getClass().getName()
+					  +": "+e.getMessage());
+			  System.exit(0);
+		  }
+	  }
+  }
+  
+  //This method updates information inside playData 
+  //un is user name, tm is time taken in second and sc is score
+  public void updateTable(String un,double tm, int sc) {
 	  
   }
   
-  //This method checks if the user name is taken
-  public boolean ifNameExists(String name) {
-	  return true;
+  //This method retrieves data from the database
+  public void retrieve() {
+	  
+  }
+  
+  //This method checks if the user name is taken. If name doesn't
+  //exist, it returns -1
+  public int nameId(String name) {
+	  try {
+	  connect();
+	  stmt = c.createStatement();
+	  ResultSet rs = stmt.executeQuery("SELECT * FROM players;");
+	  while (rs.next()) {
+		  int id = rs.getInt("id");
+		  String userName = rs.getString("userName");
+		  if (userName.toLowerCase().equals(name.toLowerCase())) {
+			  return id;
+		  }
+	  	} rs.close();stmt.close();c.close();
+	  	
+	  } catch(Exception e){
+		  System.out.println("There's an error:"+e.getClass().getName()
+				  +": "+e.getMessage());
+		  System.exit(0);
+	  }
+	  return -1;
   }
   
   //This method creates a table in the database for storing players' user names
