4a9efe90323520208b5b53366abdd0384e216f0d
李肇男 <32943976+LIZHAONAN@users.noreply.github.com>
2017-11-19 19:26:18 -0500

    Add files via upload

---
+++ GameTimer.java
@@ -0,0 +1,24 @@
+/**
+ * GameTimer is an object for recording time taken for each game
+ * @author Zhaonan Li
+ */
+
+public class GameTimer {
+	
+	public long startTime;
+	public long endTime;
+	
+	public void start() {
+		startTime = System.currentTimeMillis();
+	}
+	
+	public void end() {
+		endTime = System.currentTimeMillis();
+	}
+	
+	public double getTime() {
+		double time = (double) (endTime - startTime)/1000;
+		time = Double.parseDouble(String.format("%.1f", time));
+		return time;
+	}
+}
