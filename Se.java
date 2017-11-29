8ce95a22caebe0b6abcc187a9944c11cc013c84a
Bonnie <33436192+jingmanli0417@users.noreply.github.com>
2017-11-29 10:12:06 -0500

    Add files via upload

---
+++ Se.java
@@ -0,0 +1,42 @@
+/*
+reference: https://www.ntu.edu.sg/home/ehchua/programming/java/J8c_PlayingSound.html
+*/
+import java.io.*;
+import java.net.URL;
+import javax.sound.sampled.*;
+public class Se {
+  private Clip clip;
+  private Mixer mixer;
+  public Se() {
+    makeSound();
+  }
+  public void makeSound() {
+    Mixer.Info[] mixInfos = AudioSystem.getMixerInfo();
+    mixer = AudioSystem.getMixer(mixInfos[0]);
+    DataLine.Info dataInfo = new DataLine.Info(Clip.class, null);
+    try {
+      clip = (Clip)mixer.getLine(dataInfo);
+    } catch(Exception e) {
+      System.out.println(e);
+    }
+    try {
+      URL soundURL = Se.class.getResource("/Boom.wav");
+      AudioInputStream audioStream = AudioSystem.getAudioInputStream(soundURL);
+      clip.open(audioStream);
+    } catch(Exception e) {
+      System.out.println(e);
+    }
+
+    clip.start();
+    do {
+      try {
+        Thread.sleep(50);
+      } catch (Exception e){
+        System.out.println(e);
+      }
+    } while(clip.isActive());
+  }
+  public static void main(String[] args) {
+    new Se();
+  }
+}
