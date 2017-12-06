f055f6d5d1625c6434346d4e80d857d6cbf10eb3
lightnesscaster <leinadj24@brandeis.edu>
2017-12-06 18:57:58 -0500

    Update TicTacToeGame.java
    Refactored code.

--- CS11a_Final_Project/TicTacToeGame.java
+++ CS11a_Final_Project/TicTacToeGame.java
@@ -1,15 +1,19 @@
 import java.util.*;
 
 public class TicTacToeGame{
-  static String[][] board = {{"0","0","0"},{"0","0","0"},{"0","0","0"}};
+  public static String[][] board = {{"0","0","0"},{"0","0","0"},{"0","0","0"}};
+  public static Scanner scan = new Scanner(System.in);
+  public static String p1 = "";
+  public static String p2 = "";
+  public static int x = 0;
+  public static int y = 0;
+  public static boolean b = false;
+  public static String s = "";
+  public static int count = 0;
+  public static String p = "";
+  public static boolean c = false;
 
   public static void main(String[] args){
-    int count = 0;
-    boolean b = false;
-    Scanner scan = new Scanner(System.in);
-    String s = "";
-    String p1 = "";
-    String p2 = "";
 
     System.out.println("Welcome to Tic Tac Toe! Obtain three squares in a row to win!");
 
@@ -24,37 +28,18 @@ public class TicTacToeGame{
       }
     }while(b);
 
-    b = true;
+    playGame();
+
+
+  }
+
+  public static void playGame(){
 
     do{
-      System.out.println("Enter the coordinates (as integers) of where you wish to play: ");
-      if(scan.hasNextInt()){
-        int x = scan.nextInt();
-        if(scan.hasNextInt()){
-          int y = scan.nextInt();
-          if(count%2==0){
-            board[x][y] = "O";
-            s = p1;
-          }
-          else{
-            board[x][y] = "X";
-            s = p2;
-          }
-          count++;
-          b = gameOver();
-          printBoard();
-        }
-        else{
-          b = false;
-          scan.next();
-        }
-      }
-      else{
-        b = false;
-        scan.next();
-      }
+      boolean b = playOneMove();
 
     }while(!b);
+
     System.out.println(s+" wins!");
   }
 
@@ -79,116 +64,163 @@ public class TicTacToeGame{
   public static boolean gameOver(){
     for(int i = 0; i< board[0].length; i++){
       for(int j = 0; j< board[1].length; j++){
-        if(board[i][j].equals("O")){
-          if(i==2){
-            if(board[i-1][j].equals("O")){
-              if(board[i-2][j].equals("O")){
-                return(true);
-              }
-            }
-          }
-        }
-        if(board[i][j].equals("X")){
-          if(i==2){
-            if(board[i-1][j].equals("X")){
-              if(board[i-2][j].equals("X")){
-                return(true);
-              }
-            }
-          }
-        }
-        if(board[i][j].equals("O")){
-          if(i==0){
-            if(board[i+1][j].equals("O")){
-              if(board[i+2][j].equals("O")){
-                return(true);
-              }
-            }
-          }
-        }
-        if(board[i][j].equals("X")){
-          if(i==0){
-            if(board[i+1][j].equals("X")){
-              if(board[i+2][j].equals("X")){
-                return(true);
-              }
-            }
-          }
-        }
-        if(board[i][j].equals("O")){
-          if(j==0){
-            if(board[i][j+1].equals("O")){
-              if(board[i][j+2].equals("O")){
-                return(true);
-              }
-            }
-          }
-        }
-        if(board[i][j].equals("X")){
-          if(j==0){
-            if(board[i][j+1].equals("X")){
-              if(board[i][j+2].equals("X")){
-                return(true);
-              }
-            }
-          }
-        }
-        if(board[i][j].equals("O")){
-          if(j==2){
-            if(board[i][j-1].equals("O")){
-              if(board[i][j-2].equals("O")){
-                return(true);
-              }
-            }
-          }
-        }
-        if(board[i][j].equals("X")){
-          if(j==2){
-            if(board[i][j-1].equals("X")){
-              if(board[i][j-2].equals("X")){
-                return(true);
-              }
-            }
-          }
-        }
-        if(board[i][j].equals("O")){
-          if(i==0&&j==0){
-            if(board[i+1][j+1].equals("O")){
-              if(board[i+2][j+2].equals("O")){
-                return(true);
-              }
-            }
-          }
-        }
-        if(board[i][j].equals("X")){
-          if(i==0&&j==0){
-            if(board[i+1][j+1].equals("X")){
-              if(board[i+2][j+2].equals("X")){
-                return(true);
-              }
-            }
-          }
-        }
-        if(board[i][j].equals("O")){
-          if(i==2&&j==2){
-            if(board[i-1][j-1].equals("O")){
-              if(board[i-2][j-2].equals("O")){
-                return(true);
-              }
-            }
-          }
-        }
-        if(board[i][j].equals("X")){
-          if(i==2&&j==2){
-            if(board[i-1][j-1].equals("X")){
-              if(board[i-2][j-2].equals("X")){
-                return(true);
-              }
-            }
+        if(!(board[i][j].equals("0"))){
+          String a = board[i][j];
+          boolean b = boardSearch(i,j,a);
+          if(b){
+            return true;
           }
         }
       }
     }
     return false;
+  }
+
+  public static boolean boardSearch(int i, int j, String a){
+    boolean b = horizontalSearch(i, j, a);
+    if(b){
+      return true;
+    }
+
+    b = verticalSearch(i,j,a);
+    if(b){
+      return true;
+    }
+
+    b = diagonalSearch(i,j,a);
+    if(b){
+      return true;
+    }
+
+    return false;
+  }
+  public static boolean horizontalSearch(int i, int j, String a){
+    if(j==0){
+      if(board[i][j+1].equals(a)){
+        if(board[i][j+2].equals(a)){
+          return(true);
+        }
+      }
+    }
+    if(j==2){
+      if(board[i][j-1].equals(a)){
+        if(board[i][j-2].equals(a)){
+          return(true);
+        }
+      }
+    }
+    return false;
+  }
+
+  public static boolean verticalSearch(int i, int j, String a){
+    if(i==2){
+      if(board[i-1][j].equals(a)){
+        if(board[i-2][j].equals(a)){
+          return(true);
+        }
+      }
+    }
+    if(i==0){
+      if(board[i+1][j].equals(a)){
+        if(board[i+2][j].equals(a)){
+          return(true);
+        }
+      }
+    }
+    return false;
+  }
+
+  public static boolean diagonalSearch(int i, int j, String a){
+    if(i==0&&j==0){
+      if(board[i+1][j+1].equals(a)){
+        if(board[i+2][j+2].equals(a)){
+          return(true);
+        }
+      }
+    }
+    if(i==2&&j==2){
+      if(board[i-1][j-1].equals(a)){
+        if(board[i-2][j-2].equals(a)){
+          return(true);
+        }
+      }
+    }
+    if(i==2&&j==0){
+      if(board[i-1][j+1].equals(a)){
+        if(board[i-2][j+2].equals(a)){
+          return(true);
+        }
+      }
+    }
+    return false;
+  }
+
+  public static boolean playOneMove(){
+
+    do{
+      if(count%2==0){
+        p = p1;
+      }
+      else{
+        p = p2;
+      }
+      System.out.println(p + ", enter the coordinates (as integers) of where you wish to play: ");
+      getCoordinates();
+      count++;
+      b = gameOver();
+      printBoard();
+
+    }while(!b);
+
+
+    return b;
+  }
+
+  public static void getCoordinates(){
+    if(scan.hasNextInt()){
+      x = scan.nextInt();
+      c = false;
+      getSecondCoordinate();
+    }
+    else{
+      b = false;
+      scan.next();
+    }
+  }
+
+  public static void getSecondCoordinate(){
+    do{
+      if(scan.hasNextInt()){
+        readSecondInput();
+      }
+      else{
+        b = false;
+        scan.next();
+      }
+    }while(!c);
+  }
+
+  public static void readSecondInput(){
+    y = scan.nextInt();
+    if((x>0&&x<4)&&(y>0&&y<4)){
+      x--;
+      y--;
+      if(board[x][y].equals("0")){
+          if(p.equals(p1)){
+            board[x][y] = "O";
+          }
+          else{
+            board[x][y] = "X";
+          }
+          c = true;
+        }
+        else{
+          System.out.println("Enter valid coordinates.");
+        }
+      }
+    else{
+      System.out.println("Enter valid coordinates. ");
+      c = false;
+    }
   }
 }
